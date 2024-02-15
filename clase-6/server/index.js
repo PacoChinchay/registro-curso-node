ce695e62b54f49e9a5f947cfe6c47928d6d0e56c
PacoChinchay <pacochinchay350@gmail.com>
2024-02-14 19:04:01 -0500

    add chat en vivo con websockets

---
+++ clase-6/server/index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
+
+io.on('connection', (socket) => {
+  console.log('a user has connectec')
+
+  socket.on('disconnect', () => {
+    console.log('an user has disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  })
+})
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`)
+})
